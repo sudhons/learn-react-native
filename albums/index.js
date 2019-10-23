3314a5bb0469241dd7baec2e21646fef4745e15a
sudhons <sudhons@gmail.com>
2019-10-23 23:24:33 +0100

    create albums header component

--- albums/index.js
+++ albums/index.js
@@ -6,10 +6,11 @@
 // import App from './App';
 import {name as appName} from './app.json';
 import React from 'react';
-import {AppRegistry, Text} from 'react-native';
+import {AppRegistry} from 'react-native';
+import Header from './src/components/Header';
 
 const App = () => {
-  return <Text>Some Text</Text>;
+  return <Header headerText="Albums"/>
 };
 
 AppRegistry.registerComponent(appName, () => App);
