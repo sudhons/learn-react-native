1e27247e66d00462e85a24d8f1636f08bb0814e5
sudhons <sudhons@gmail.com>
2019-10-26 07:29:31 +0100

    set up auth react native project

---
+++ auth/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 're`act-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>An app</Text>
+      </View>
+    )
+  }
+}
