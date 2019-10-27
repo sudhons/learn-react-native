e5a83dfe336026e9ddc93e6a880f4285a35b4a66
sudhons <sudhons@gmail.com>
2019-10-27 15:15:25 +0100

    set up stack_tech react native project with redux

---
+++ tech_stack/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers';
+import {Header} from './components/common';
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View>
+        <Header headerText="Tech Stack" />
+        <Text>Hi</Text>
+      </View>
+    </Provider>
+  );
+};
+
+export default App;
