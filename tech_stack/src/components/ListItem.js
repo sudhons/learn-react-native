3f679d70653f1a4aa6daaed70fffda46ae3edb88
sudhons <sudhons@gmail.com>
2019-10-29 07:55:49 +0100

    implement selecting and showing a tech stack decription

--- tech_stack/src/components/ListItem.js
+++ tech_stack/src/components/ListItem.js
@@ -1,16 +1,63 @@
 import React, {Component} from 'react';
-import {Text} from 'react-native';
+import {
+  Text,
+  TouchableWithoutFeedback,
+  View,
+  LayoutAnimation,
+} from 'react-native';
+import {connect} from 'react-redux';
 import {CardSection} from './common';
+import * as actions from '../actions';
 
-export default class ListItem extends Component {
+class ListItem extends Component {
+  componentWillUpdate() {
+    LayoutAnimation.spring();
+  }
+
+  renderDiscription() {
+    const {
+      library: {description},
+      expanded,
+    } = this.props;
+
+    if (expanded) {
+      return (
+        <CardSection>
+          <Text>{description}</Text>
+        </CardSection>
+      );
+    }
+  }
+
   render() {
+    const {
+      selectLibrary,
+      library: {id, title},
+    } = this.props;
+
+    const {titleStyle} = styles;
+
     return (
-      <CardSection>
-        <Text style={styles.titleStyle}>{this.props.library.title}</Text>
-      </CardSection>
+      <TouchableWithoutFeedback onPress={() => selectLibrary(id)}>
+        <View>
+          <CardSection>
+            <Text style={titleStyle}>{title}</Text>
+          </CardSection>
+          {this.renderDiscription()}
+        </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
+
+const mapStateToProps = ({selectedLibraryId}, {library}) => {
+  return {expanded: selectedLibraryId === library.id};
+};
+
+export default connect(
+  mapStateToProps,
+  actions,
+)(ListItem);
 
 const styles = {
   titleStyle: {
