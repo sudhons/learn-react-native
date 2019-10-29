ddaa484560a604d0a7cc1fcf35f1607fb3f03abd
sudhons <sudhons@gmail.com>
2019-10-29 13:25:12 +0100

    set up firebase authentication

---
+++ manager/firebase-config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyB_p4GvOkz2EORdeujMGzWqhOO6HQ0p9WM',
+  authDomain: 'manager-002.firebaseapp.com',
+  databaseURL: 'https://manager-002.firebaseio.com',
+  projectId: 'manager-002',
+  storageBucket: 'manager-002.appspot.com',
+  messagingSenderId: '879281971300',
+  appId: '1:879281971300:web:6b78dda30d06f38b85ff71',
+};
--- manager/src/App.js
+++ manager/src/App.js
@@ -2,9 +2,15 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
+import firease from 'firebase';
 import reducers from './reducers';
+import firebaseAuth from '../firebase-config';
 
 export default class App extends Component {
+  componentDidMount() {
+    firease.initializeApp(firebaseAuth);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
