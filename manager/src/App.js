c4d0d09fe4ddef9356ad1d16136e2f87ab906db2
sudhons <sudhons@gmail.com>
2019-10-29 13:16:16 +0100

    set up manager react native project with redux

---
+++ manager/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import reducers from './reducers';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>Hello!</Text>
+        </View>
+      </Provider>
+    );
+  }
+}
