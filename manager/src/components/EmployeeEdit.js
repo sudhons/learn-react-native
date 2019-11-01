8a851b4eca2f63fd36d99258ae2bb8897e63e2aa
sudhons <sudhons@gmail.com>
2019-11-01 12:51:57 +0100

    implement texting employing schedule

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -3,6 +3,7 @@ import {Card, CardSection, Button} from './common';
 import {connect} from 'react-redux';
 import * as actions from '../actions';
 import EmployeeForm from './EmployeeForm';
+import Communcations from 'react-native-communications';
 
 class EmployeeEdit extends Component {
   componentDidMount() {
@@ -18,12 +19,21 @@ class EmployeeEdit extends Component {
     employeeSave({name, phone, shift, employeeId: uid});
   };
 
+  onTextPress = () => {
+    const { phone, shift } = this.props;
+
+    Communcations.text(phone, `You coming shift is on ${shift}`);
+  }
+
   render() {
     return (
       <Card>
         <EmployeeForm />
         <CardSection>
           <Button onPress={this.onButtonPress}>Save Changes</Button>
+        </CardSection>
+        <CardSection>
+          <Button onPress={this.onTextPress}>Text Schedule</Button>
         </CardSection>
       </Card>
     );
